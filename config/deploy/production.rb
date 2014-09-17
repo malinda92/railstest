ca16f8535834c198023c34c8b81fecd167c2efea
malinda92 <wow.malinda92@gmail.com>
2014-09-17 23:54:29 +0530

    changes in production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,8 @@ set :branch, "master"
 # used to connect and deploy
 set :deploy_user, "railsadmin"
 # the 'full name' of the application
-set :full_app_name, "#{fetch(:application)}_#{fetch(:stage)}"
+# set :full_app_name, "#{fetch(:application)}_#{fetch(:stage)}"
+set :full_app_name, “railsapptest”
 # the server(s) to deploy to
 server 'railsapptest.cloudapp.net', user: 'railsadmin', roles: %w{web app db}, primary: true
 # the path to deploy to
